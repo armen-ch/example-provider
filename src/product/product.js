146e24997bdc8b7142ad72f16991755c7383c025
achuljyan <achuljyan@questrade.com>
2020-11-19 21:26:31 +0000

    added name along with names to provide backcompatibility

--- src/product/product.js
+++ src/product/product.js
@@ -1,8 +1,9 @@
 class Product {
-    constructor(id, type, names, version) {
+    constructor(id, type, names, name, version) {
         this.id = id;
         this.type = type;
         this.names = names;
+        this.name = name;
         this.version = version;
     }
 }
