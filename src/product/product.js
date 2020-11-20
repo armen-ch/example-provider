97dcf59f777383279d54f8d10150ac87f8b4520e
achuljyan <achuljyan@questrade.com>
2020-11-20 13:39:22 +0000

    names => name

--- src/product/product.js
+++ src/product/product.js
@@ -2,7 +2,7 @@ class Product {
     constructor(id, type, name, version) {
         this.id = id;
         this.type = type;
-        this.names = name;
+        this.name = name;
         this.version = version;
     }
 }
